f7f1fc66b23ac2cc22d53650ec485654332e362c
Jakob Holst <lyngknuden@gmail.com>
2022-12-09 11:25:59 +0100

    pretty log

--- bin/awx_manager.py
+++ bin/awx_manager.py
@@ -204,7 +204,7 @@ def awx_create_host(name, description, inventory, organization):
   headers = {"User-agent": "python-awx-client", "Content-Type": "application/json","Authorization": "Bearer {}".format(mytoken)}
   url ="https://ansible.openknowit.com/api/v2/hosts/"
   resp = requests.post(url,headers=headers, json=data)
-  response =  str(resp)
+  response = json.loads(resp.content)
   prettyllog("manage", "host", name, organization, response)
 
 
